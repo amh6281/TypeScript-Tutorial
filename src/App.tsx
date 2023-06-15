a42357fc4a5db957c281f91083d1e6f95c9f1a7f
amh6281 <amh6281@naver.com>
2023-06-15 20:48:20 +0900

    변수 타입 선언 2022.06.15
    number, string, boolean, array, object, any, union type에 대한 공부

---
+++ src/App.tsx
@@ -0,0 +1,86 @@
+function App() {
+  let variable = "hello";
+  variable = "hihi";
+
+  let age = 26;
+  // age = "eighteen";
+  let ageType: number;
+  ageType = 26;
+
+  let testString: string;
+  testString = "test";
+
+  let testBoolean: boolean;
+  testBoolean = true;
+
+  let testStringOrNumber: string | number;
+  testStringOrNumber = "10";
+  testStringOrNumber = 10;
+
+  //Array
+  let names = ["one", "two", "three"];
+  names.push("hello");
+
+  let numbers = [11, 22, 33];
+  numbers.push(44);
+
+  let testStringArray: string[];
+  testStringArray = ["one", "two"];
+
+  let testNumberArray: number[];
+  testNumberArray = [1, 2, 3];
+
+  let testStringOrNumberArray: (string | number)[]; // union type
+  testStringOrNumberArray = [1, "two", 3];
+
+  // Object
+  let user = {
+    username: "jane",
+    age: 22,
+    isAdmin: false,
+  };
+  user.username = "john";
+  user.age = 26;
+  // user.isAdmin = "true"
+  user.isAdmin = true;
+  // user.phone="123123" // error
+
+  let userObject: {
+    username: string;
+    age: number;
+    isAdmin: boolean;
+  };
+  userObject = {
+    username: "jane",
+    age: 26,
+    isAdmin: true,
+    // phone:"123123" // error
+  };
+
+  let userObject2: {
+    username: string;
+    age: number;
+    isAdmin: boolean;
+    phone?: string;
+  };
+  userObject2 = {
+    username: "john",
+    age: 26,
+    isAdmin: true,
+    phone: "0101010",
+  };
+
+  //Any
+  let testAny: any;
+  testAny = 123;
+  testAny = "hello";
+  testAny = true;
+  testAny = [true];
+  testAny = {};
+  let testAnyArray: any[];
+  testAnyArray = [1, "two", false, []];
+
+  return <>hello</>;
+}
+
+export default App;
